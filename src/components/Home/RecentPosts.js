a3bfb30dcdc46d6ca6e47545f683a963ab61cddd
iuritorres <iuri.t1000@gmail.com>
2023-11-27 18:24:51 -0300

    refactor: adjust links paths

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -11,7 +11,7 @@ const RecentPosts = ({ blogs }) => {
           Mais recentes
         </h2>
         <Link
-          href="/categories/all"
+          href="/categories/todos"
           className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2      text-base md:text-lg"
         >
           Ver tudo
