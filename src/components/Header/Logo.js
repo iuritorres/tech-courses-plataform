904d25603199267821629a381ab28972ccec4511
iuritorres <iuri.t1000@gmail.com>
2023-11-30 13:52:57 -0300

    refactor: remove and rename logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
 import Link from 'next/link'
-import bytecodeLogo from '@/public/logo-svg.svg'
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-      <div className="w-12 md:w-16 rounded-full overflow-hidden border-dark dark:border-gray  mr-2 md:mr-4">
+      {/* <div className="w-12 md:w-16 rounded-full overflow-hidden border-dark dark:border-gray  mr-2 md:mr-4">
         <Image
           src={bytecodeLogo}
           alt="CodeBucks logo"
@@ -13,9 +11,11 @@ const Logo = () => {
           sizes="20vw"
           priority
         />
-      </div>
-      <span className="font-bold dark:font-semibold text-lg md:text-xl">
-        ByteCode
+      </div> */}
+
+      <span className="font-bold dark:font-semibold text-2xl md:text-4xl">
+        Rota
+        <span className="text-accent dark:text-accentDark font-bold">Tech</span>
       </span>
     </Link>
   )
